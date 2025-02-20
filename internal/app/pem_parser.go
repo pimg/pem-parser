3252827805c783c470c7b1f1dc9401e15295f178
pimg <pim.gaemers@gmail.com>
2025-02-20 12:52:05 +0100

    feat: parsed fields seperate for better overview
    Issue: #15

--- internal/app/pem_parser.go
+++ internal/app/pem_parser.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"log/slog"
 	"strings"
+	"time"
 
 	"github.com/smallstep/certinfo"
 )
@@ -15,8 +16,26 @@ type PEMHandler struct {
 }
 
 type PEMResponse struct {
-	Result string
-	Type   string
+	SerialNumber            string
+	DistinguishedName       DistinguishedName
+	IssuerName              string
+	NotBefore               time.Time
+	NotAfter                time.Time
+	SubjectAlternativeNames []string
+	KeyUsages               []string
+	Raw                     string
+	Type                    string
+}
+
+type DistinguishedName struct {
+	CommonName         string
+	SerialNumber       string
+	Country            []string
+	State              []string
+	Locality           []string
+	Organization       []string
+	OrganizationalUnit []string
+	EmailAddress       []string
 }
 
 func NewPEMHandler(logger *slog.Logger) *PEMHandler {
@@ -41,11 +60,27 @@ func (h *PEMHandler) Handle(pemRaw []byte) (*PEMResponse, error) {
 			h.logger.Error("failed to parse certificate", "error", err)
 			return nil, err
 		}
-		pemResponse.Result, err = certinfo.CertificateText(cert)
+		pemResponse.Raw, err = certinfo.CertificateText(cert)
 		if err != nil {
 			h.logger.Error("failed to render certificate text", "error", err)
 			return nil, err
 		}
+
+		pemResponse.SerialNumber = cert.SerialNumber.String()
+		pemResponse.IssuerName = cert.Issuer.CommonName
+		pemResponse.NotBefore = cert.NotBefore
+		pemResponse.NotAfter = cert.NotAfter
+		pemResponse.DistinguishedName = DistinguishedName{
+			CommonName:         cert.Subject.CommonName,
+			SerialNumber:       cert.Subject.SerialNumber,
+			Country:            cert.Subject.Country,
+			State:              cert.Subject.Province,
+			Locality:           cert.Subject.Locality,
+			Organization:       cert.Subject.Organization,
+			OrganizationalUnit: cert.Subject.OrganizationalUnit,
+		}
+		pemResponse.SubjectAlternativeNames = cert.DNSNames
+		pemResponse.KeyUsages = parseKeyUsage(cert.KeyUsage)
 	case "CERTIFICATE REQUEST":
 		csr, err := x509.ParseCertificateRequest(block.Bytes)
 		if err != nil {
@@ -53,11 +88,21 @@ func (h *PEMHandler) Handle(pemRaw []byte) (*PEMResponse, error) {
 			return nil, err
 		}
 
-		pemResponse.Result, err = certinfo.CertificateRequestText(csr)
+		pemResponse.Raw, err = certinfo.CertificateRequestText(csr)
 		if err != nil {
 			h.logger.Error("failed to render certificate request text", "error", err)
 			return nil, err
 		}
+		pemResponse.DistinguishedName = DistinguishedName{
+			CommonName:         csr.Subject.CommonName,
+			SerialNumber:       csr.Subject.SerialNumber,
+			Country:            csr.Subject.Country,
+			State:              csr.Subject.Province,
+			Locality:           csr.Subject.Locality,
+			Organization:       csr.Subject.Organization,
+			OrganizationalUnit: csr.Subject.OrganizationalUnit,
+		}
+		pemResponse.SubjectAlternativeNames = csr.DNSNames
 	default:
 		h.logger.Info("unknown certificate type", "type", block.Type)
 		if strings.Contains(block.Type, "PRIVATE") {
@@ -69,4 +114,36 @@ func (h *PEMHandler) Handle(pemRaw []byte) (*PEMResponse, error) {
 	pemResponse.Type = block.Type
 
 	return pemResponse, nil
+}
+
+func parseKeyUsage(usage x509.KeyUsage) []string {
+	var keyUsages []string
+	if usage&x509.KeyUsageDigitalSignature != 0 {
+		keyUsages = append(keyUsages, "DigitalSignature")
+	}
+	if usage&x509.KeyUsageContentCommitment != 0 {
+		keyUsages = append(keyUsages, "ContentCommitment")
+	}
+	if usage&x509.KeyUsageKeyEncipherment != 0 {
+		keyUsages = append(keyUsages, "KeyEncipherment")
+	}
+	if usage&x509.KeyUsageDataEncipherment != 0 {
+		keyUsages = append(keyUsages, "DataEncipherment")
+	}
+	if usage&x509.KeyUsageKeyAgreement != 0 {
+		keyUsages = append(keyUsages, "KeyAgreement")
+	}
+	if usage&x509.KeyUsageCertSign != 0 {
+		keyUsages = append(keyUsages, "CertSign")
+	}
+	if usage&x509.KeyUsageCRLSign != 0 {
+		keyUsages = append(keyUsages, "CRLSign")
+	}
+	if usage&x509.KeyUsageEncipherOnly != 0 {
+		keyUsages = append(keyUsages, "EncipherOnly")
+	}
+	if usage&x509.KeyUsageDecipherOnly != 0 {
+		keyUsages = append(keyUsages, "DecipherOnly")
+	}
+	return keyUsages
 }
--- internal/port/ui/pem_handler.go
+++ internal/port/ui/pem_handler.go
@@ -3,13 +3,38 @@ package ui
 import (
 	"fmt"
 	"net/http"
+	"pem-parser/internal/app"
 	"strings"
+	"time"
 )
 
 type PEMParserPage struct {
 	SuccessMessage string
 	ErrorMessage   string
-	Result         string
+	Result         *PEMResponse
+}
+
+type PEMResponse struct {
+	SerialNumber            string
+	DN                      DistinguishedName
+	IssuerName              string
+	NotBefore               time.Time
+	NotAfter                time.Time
+	SubjectAlternativeNames []string
+	KeyUsages               []string
+	Raw                     string
+	Type                    string
+}
+
+type DistinguishedName struct {
+	CommonName         string
+	SerialNumber       string
+	Country            string
+	State              string
+	Locality           string
+	Organization       string
+	OrganizationalUnit string
+	EmailAddress       string
 }
 
 func (s *Server) pemParserHandler(w http.ResponseWriter, r *http.Request) {
@@ -29,7 +54,7 @@ func (s *Server) pemParserHandler(w http.ResponseWriter, r *http.Request) {
 
 	if out != nil {
 		page.SuccessMessage = fmt.Sprintf("Successfully parsed PEM %s file", out.Type)
-		page.Result = out.Result
+		page.Result = mapPEMResponse(out)
 	}
 
 	err = s.Templates.ExecuteTemplate(w, "result-block", page)
@@ -46,4 +71,26 @@ func parseForm(r *http.Request) ([]byte, error) {
 	}
 
 	return []byte(strings.TrimSpace(r.PostFormValue("pem"))), nil
+}
+
+func mapPEMResponse(response *app.PEMResponse) *PEMResponse {
+	return &PEMResponse{
+		SerialNumber: response.SerialNumber,
+		DN: DistinguishedName{
+			CommonName:         response.DistinguishedName.CommonName,
+			SerialNumber:       response.DistinguishedName.SerialNumber,
+			Country:            strings.Join(response.DistinguishedName.Country, ","),
+			State:              strings.Join(response.DistinguishedName.State, ","),
+			Locality:           strings.Join(response.DistinguishedName.Locality, ","),
+			Organization:       strings.Join(response.DistinguishedName.Organization, ","),
+			OrganizationalUnit: strings.Join(response.DistinguishedName.OrganizationalUnit, ","),
+			EmailAddress:       strings.Join(response.DistinguishedName.EmailAddress, ","),
+		},
+		IssuerName:              response.IssuerName,
+		NotBefore:               response.NotBefore,
+		NotAfter:                response.NotAfter,
+		SubjectAlternativeNames: response.SubjectAlternativeNames,
+		KeyUsages:               response.KeyUsages,
+		Raw:                     response.Raw,
+	}
 }
