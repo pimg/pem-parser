7bb999854591b99ccee808aabfd8fa67448f67cb
Niels Dequeker <niels@dqkr.be>
2025-02-28 12:58:56 +0100

    fix: drop firefox
    * Remove default whitespace inside the textarea
    
    * Fix dropping a PEM file in Firefox
    
    * Display placeholder for the PEM file
    
    * Show drop indicator when dragging a file over the textarea
    
    ---------
    
    Co-authored-by: ndequeker <niels@nd-software.be>
    Co-authored-by: Pim Gaemers <pimg@users.noreply.github.com>

--- internal/port/ui/assets/js/form-utils.js
+++ internal/port/ui/assets/js/form-utils.js
@@ -35,6 +35,23 @@ function isLikelyPEM(input) {
     return pemRegex.test(input);
 }
 
+// required to make drop work on Firefox
+pem.ondragover = function(e) {
+    e.preventDefault();
+
+    if (!e.target.classList.contains('drop-indicator')) {
+        e.target.classList.add('drop-indicator')
+    }
+}
+
+pem.ondragend = function (e) {
+    e.target.classList.remove('drop-indicator')
+}
+
+pem.ondragleave = function (e) {
+    e.target.classList.remove('drop-indicator')
+}
+
 pem.ondrop = function (e) {
     e.preventDefault();
     const file = e.dataTransfer.files[0];
